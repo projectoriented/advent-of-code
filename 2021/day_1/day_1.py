20b84ba0020031a3cc81470fcbb64445e471474a
Mei Wu <25568561+projectoriented@users.noreply.github.com>
2022-12-17 11:32:58 -0800

    compltd part2

--- 2021/day_1/day_1.py
+++ 2021/day_1/day_1.py
@@ -5,6 +5,7 @@
 import os
 import more_itertools as mit
 
+## Part 1.
 # Resolve input file path
 fp = os.path.join(os.path.dirname(os.path.realpath(__file__)), "day_1-data_input-mei.txt")
 
@@ -21,5 +22,13 @@ def is_increased(first: int, second: int) -> int:
 sum_of_increased=0
 overlapping_chunks = list(mit.windowed(data, n=2, step=1))
 for c in overlapping_chunks:
+    sum_of_increased+=is_increased(*c)
+print(sum_of_increased)
+
+## Part 2.
+windowed_sums = [sum(x) for x in list(mit.windowed(data, n=3, step=1))]
+sum_chunks = list(mit.windowed(windowed_sums, n=2, step=1))
+sum_of_increased=0
+for c in sum_chunks:
     sum_of_increased+=is_increased(*c)
 print(sum_of_increased)
