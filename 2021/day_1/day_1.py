e597d28716da3430f343ac3e986186424a14ba18
Mei Wu <25568561+projectoriented@users.noreply.github.com>
2022-12-17 10:35:50 -0800

    day one complete

--- 2021/day_1/day_1.py
+++ 2021/day_1/day_1.py
@@ -1,3 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # https://adventofcode.com/2021/day/1
+
+import os
+import more_itertools as mit
+
+# Resolve input file path
+fp = os.path.join(os.path.dirname(os.path.realpath(__file__)), "day_1-data_input-mei.txt")
+
+# Save each line into a variable
+with open (fp) as file_object:
+    data = [int(line.rstrip()) for line in file_object]
+
+def is_increased(first: int, second: int):
+    if second > first:
+        return 1
+    else:
+        return 0
+
+starting=0
+overlapping_chunks = list(mit.windowed(data, n=2, step=1))
+for c in overlapping_chunks:
+    starting+=is_increased(*c)
+print(starting)
